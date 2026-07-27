285464904dbb879ab859eefeabda699ed22e93c5
Brian Lin <shihhsu1@ualberta.ca>
2026-07-27 17:35:46 +0000

    refactor: update inference error handling to ignore null values
    Signed-off-by: Brian Lin <shihhsu1@ualberta.ca>

--- src/text2sql_eval_toolkit/inference/agentic_pipeline.py
+++ src/text2sql_eval_toolkit/inference/agentic_pipeline.py
@@ -2038,7 +2038,7 @@ Analyze the error and generate a corrected SQL query. If you need more schema in
                     pred = existing["predictions"].get(pipeline_id)
                     if pred:
                         # Always retry if there was an inference error (unless skip flag is set)
-                        if "inference_error" in pred and not skip_inference_error_retries:
+                        if pred.get("inference_error") and not skip_inference_error_retries:
                             logger.info(
                                 f"Retrying failed inference for id={question_id}, pipeline={pipeline_id}"
                             )
